14ad06bdac64d43c2f4c6eaeebf09cda5d719438
nepynepp <choice.nnn@gmail.com>
2016-05-11 19:25:33 +0700

    add step

--- routes/step.js
+++ routes/step.js
@@ -3,8 +3,22 @@ var router = express.Router();
 var axios = require('axios');
 
 
-router.get('/', function(req, res, next) {
-    return res.render('step', {step:1111})
+router.get('/:year/:month/:day', function(req, res, next) {
+    var token = req.session.token;
+    var user_id = req.session.user_id;
+    var date = req.params.year + '-' + req.params.month + '-' + req.params.day;
+
+    axios.get('https://api.fitbit.com/1/user/' + user_id + '/activities/date/' + date + '.json',
+        {headers: {'Authorization': 'Bearer ' + token}})
+        .then(function (response) {
+            console.log(response.data.goals.steps)
+            return res.render('step', {step:response.data.goals.steps})
+
+        })
+        .catch(function (response) {
+            return res.json({data:response});
+        });
+
 });
 
 
